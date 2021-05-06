b43df6088c5dd8e1eddee236a269b745556f8ca3
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-05-06 20:13:39 +0530

    [0.0.2] Board componrnt partially done, props passed='squares, onClick'

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,14 +1,14 @@
-// import React from "react"
-// import Square from "./Square"
+import React from "react"
+import Square from "./Square"
 
-// function Board({ squares, onClick }) {
-// 	return (
-// 		<div className="board">
-// 			{squares.map((square, index) => (
-// 				<Square key={index} value={square} onClick={() => onClick(index)} />
-// 			))}
-// 		</div>
-// 	)
-// }
+function Board({ squares, onClick }) {
+	return (
+		<div className="board">
+			{squares.map((square, index) => (
+				<Square key={index} value={square} onClick={() => onClick(index)} />
+			))}
+		</div>
+	)
+}
 
-// export default Board
+export default Board
